f5f012bcb1567d84b57cead148d83747f94c6056
hana <hanasabih@gmail.com>
2023-04-03 13:46:00 +0300

    database-performance

--- schema.sql
+++ schema.sql
@@ -65,3 +65,9 @@ CREATE TABLE visits (
   FOREIGN KEY (vet_id) REFERENCES vets (id) ON DELETE CASCADE,
   FOREIGN KEY (animal_id) REFERENCES animals (id) ON DELETE CASCADE
 );
+
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+
+
+
